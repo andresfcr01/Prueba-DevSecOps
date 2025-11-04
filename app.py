1dffb04013646b6d3eacca7ab4d552fd25cf9960
Andrés Castiblanco <andresfcr01@users.noreply.github.com>
2025-11-03 20:34:34 -0500

    fix: Credenciales de Docker verificadas y actualizadas

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     # Línea modificada para forzar un nuevo build:
-    return 'Hello, World DevSecOps! - Versión GitOps Final 🚀' 
+    return 'Hello, World DevSecOps! - Versión GitOps Final 2.0🚀' 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
